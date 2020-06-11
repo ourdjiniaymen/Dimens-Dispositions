33ef7bb1b796cd01b18c802756deefc7d19fa62d
ourdjini aymen <ga_ourdjini@esi.dz>
2020-06-11 13:25:45 +0100

    simple Android application that uses different dimensions and layouts

---
+++ app/src/main/java/com/example/dimens_dispositions/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.dimens_dispositions
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*get devise size
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels*/
+
+
+    }
+}
